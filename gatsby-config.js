21e0ad35ebeadda4d13e7728887f642f24cf5d95
isaksolheim <isak.m.h.s@gmail.com>
2021-11-16 22:45:04 +0100

    Add image of mule to landing page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,19 @@
+const path = require('path');
+
 module.exports = {
   siteMetadata: {
-    siteUrl: 'https://www.yourdomain.tld',
+    siteUrl: 'https://muldyr.no',
     title: 'muldyr.no',
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 };
