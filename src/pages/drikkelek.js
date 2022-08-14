f821bc00ed0db94be1a3483be2b76d37672c42cc
isaksolheim <mail@isaks.io>
2022-08-14 23:06:39 +0200

    Add new pages

---
+++ src/pages/drikkelek.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "./index.css";
+
+const Drikkelek = ({ data }) => (
+  <main style={{ maxWidth: "600px", margin: "0 auto" }}>
+    <strong>100 Spørmsål</strong>{" "}
+    <p>
+      Last ned 100 Spørsmål fra App Store. Lag dine egne spill og drikk med
+      omhu.
+    </p>
+  </main>
+);
+
+export default Drikkelek;
